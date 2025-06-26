247519d947b7ee02f7a0f0d78a610a3c97117ea3
Remon <r.botros@guretruck.com>
2025-06-26 23:12:32 +0300

    update links length

--- src/components/ConnectionsGraph.jsx
+++ src/components/ConnectionsGraph.jsx
@@ -102,8 +102,8 @@ const ConnectionsGraph = () => {
   useEffect(() => {
     if (forceGraphRef.current && graphData.nodes.length > 0) {
       const fg = forceGraphRef.current;
-      fg.d3Force("link").distance(150).strength(0.5);
-      fg.d3Force("charge").strength(-800).distanceMax(400);
+      fg.d3Force("link").distance(50).strength(0.5);
+      fg.d3Force("charge").strength(-800).distanceMax(150);
       fg.d3ReheatSimulation();
     }
   }, [graphData, dimensions]);
